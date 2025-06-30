85c19faa9a2b33dd26dacf6cb5fb0329e494ff06
kalyma <swnacional@gmail.com>
2025-06-29 21:33:03 -0500

    2

--- startup.sh
+++ startup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 pip install --upgrade pip
 pip install -r requirements.txt
-<<<<<<< HEAD
 python GDSkool_1_1.py
-=======
-python GDSkool_1_1.py
->>>>>>> 9d09abd429c5428a2528153061595c1fca7f00e2
